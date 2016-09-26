c515d53e7b2eedb7b5f62c03ba8d47beeebafad8
Troy Lee <troyuit@gmail.com>
2016-09-26 12:31:48 +0700

    Update layout

--- Vnap.Web/wwwroot/app/scripts/config.js
+++ Vnap.Web/wwwroot/app/scripts/config.js
@@ -9,7 +9,7 @@ vnap
             IdleProvider.idle(5); // in seconds
             IdleProvider.timeout(120); // in seconds
 
-            $urlRouterProvider.otherwise("/index/main");
+            $urlRouterProvider.otherwise("/index/plant");
             $ocLazyLoadProvider.config({
                 // Set to true if you want to see what and when is dynamically loaded
                 debug: false
